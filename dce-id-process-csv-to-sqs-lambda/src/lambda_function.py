4b6367a9ebe4768527c21e25166a8818d03dd02c
Kryvenkyi <akryvenkyi@endeavorco.com>
2021-05-07 14:05:43 +0100

    init

---
+++ dce-id-process-csv-to-sqs-lambda/src/lambda_function.py
@@ -0,0 +1,104 @@
+import json
+import csv
+
+import boto3
+import codecs
+import os
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def process_file(bucket, file, s3_client, queue, activity_queue):
+    s3_bucket = s3_client.Bucket(bucket)
+    s3_obj = s3_bucket.Object(file)
+    uploaded_time = s3_obj.last_modified
+
+    meta = compile_metadata({}, s3_obj.metadata)
+    validate_requester(meta)
+    realm = file.split('/')[2]
+    file_name = file.split('/')[3]
+    task_id = str(int(uploaded_time.timestamp() * 1000)) + '-' + file_name
+    meta['realm'] = realm
+    meta['taskId'] = task_id
+    counter = 0
+    if activity_queue:
+        send_activity_message(meta, file_name, uploaded_time, counter, activity_queue)
+    # This should stream file from s3 and process it line by line without uploading all file in memory
+    for row in csv.DictReader(codecs.getreader("utf-8")(s3_obj.get()['Body'])):
+        line = list(row.values())
+        payload = meta.copy()
+        payload['user'] = {
+            'email': line[0],
+            'name': line[1] if len(line) > 1 else None
+        }
+        # add empty header and properties because a presence of 'header' is used to determine a format of message
+        queue.send_message(MessageBody=json.dumps({'payload': payload, 'headers': {}, 'properties': {}}))
+        counter = counter + 1
+
+    logger.info(f"Sent [{counter}] create user messages")
+    s3_obj.delete()
+    if activity_queue:
+        send_activity_message(meta, file_name, uploaded_time, counter, activity_queue)
+    return counter
+
+
+def send_activity_message(meta, file_name, uploaded_time, expected_number, queue):
+    payload = meta.copy()
+    payload['taskType'] = 'CREATE_USER'
+    payload['task'] = {
+        'fileName': file_name,
+        'uploadedTime': uploaded_time.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+        'expected': expected_number
+    }
+    queue.send_message(MessageBody=json.dumps({'payload': payload, 'headers': {}, 'properties': {}}))
+
+
+def compile_metadata(r, s):
+    for key in s:
+        p = key.split('-')
+        t = {}
+        for i in p[:-1]:
+            if not r.get(i):
+                r[i] = t
+            t = r[i]
+        t[p[-1]] = s[key]
+    return r
+
+
+def validate_requester(meta):
+    requester = meta.get('requester', {})
+    expected_fields = ['exid', 'ip', 'city', 'countrycode']
+
+    for f in expected_fields:
+        if f not in requester:
+            raise AttributeError('S3 objects must be configured with requester.exid, requester.ip, requester.city, '
+                                 'requester.countrycode')
+
+
+def lambda_handler(event, context):
+
+    queue_name = os.environ['CREATE_USERS_SQS_QUEUE']
+    s3_client = boto3.resource('s3')
+
+    sqs_client = boto3.resource('sqs')
+    queue = sqs_client.get_queue_by_name(QueueName=queue_name)
+    send_bulk_activity_message = os.environ.get('SEND_BULK_ACTIVITY_MESSAGE', 'FALSE').upper() == 'TRUE'
+
+    if send_bulk_activity_message:
+        bulk_activity_queue_name = os.environ['BULK_ACTIVITY_SQS_QUEUE']
+        activity_queue = sqs_client.get_queue_by_name(QueueName=bulk_activity_queue_name)
+    else:
+        activity_queue = False
+
+    for rec in event.get('Records', []):
+        bucket = rec.get('s3').get('bucket').get('name')
+        file = rec.get('s3').get('object').get('key')
+        logger.info(f"Process [{bucket}/{file}] started")
+        try:
+            processed = process_file(bucket, file, s3_client, queue, activity_queue)
+            logger.info(f"Process [{bucket}/{file}] finished, processed [{processed}] records")
+        except Exception as e:
+            logger.error(e)
+            raise e
---
+++ dce-id-process-csv-to-sqs-lambda/tests/test_lambda_function.py
@@ -0,0 +1,141 @@
+import boto3
+import pytest
+from freezegun import freeze_time
+
+from moto import mock_s3, mock_sqs
+from ..src import lambda_function
+
+_region = 'eu-west-1'
+_bucket = 'test-dce-id-users-bulk-operation'
+_event = {
+    'Records': [
+        {
+            's3': {
+                'bucket': {'name': 'test-dce-id-users-bulk-operation'},
+                'object': {'key': 'test/create-users/dce.test/users.csv'}
+            }
+        }
+    ]
+}
+
+
+def aws_credentials(monkeypatch):
+    """Mocked AWS Credentials for moto."""
+    monkeypatch.setenv("AWS_ACCESS_KEY_ID", "testing")
+    monkeypatch.setenv("AWS_SECRET_ACCESS_KEY", "testing")
+    monkeypatch.setenv("AWS_SECURITY_TOKEN", "testing")
+    monkeypatch.setenv("AWS_SESSION_TOKEN", "testing")
+
+    """Mocked lambda environment variables"""
+    monkeypatch.setenv('CREATE_USERS_SQS_QUEUE', 'CREATE_USERS_SQS_QUEUE')
+    monkeypatch.setenv('BULK_ACTIVITY_SQS_QUEUE', 'BULK_ACTIVITY_SQS_QUEUE')
+    monkeypatch.setenv('SEND_BULK_ACTIVITY_MESSAGE', 'TRUE')
+
+
+def prepare_env(monkeypatch):
+    aws_credentials(monkeypatch)
+    s3 = boto3.client('s3', region_name=_region)
+    sqs = boto3.client('sqs', region_name=_region)
+    s3.create_bucket(Bucket=_bucket, CreateBucketConfiguration={'LocationConstraint': _region})
+
+    sqs.create_queue(QueueName='CREATE_USERS_SQS_QUEUE')
+    sqs.create_queue(QueueName='BULK_ACTIVITY_SQS_QUEUE')
+
+
+@freeze_time("2012-01-01 12:00:00")
+@mock_s3
+@mock_sqs
+def test_should_fails_if_no_requester_meta(monkeypatch):
+    prepare_env(monkeypatch)
+    s3 = boto3.client('s3', region_name=_region)
+    s3_key = 'test/create-users/dce.test/users.csv'
+    meta = {}
+    s3.put_object(Bucket=_bucket, Key=s3_key, Body='email, name\nuser1@mail.com,User1\nuser2@mail.com,User2',
+                  Metadata=meta)
+
+    # When
+    with pytest.raises(AttributeError):
+        lambda_function.lambda_handler(_event, {})
+
+
+@freeze_time("2012-01-01 12:00:00")
+@mock_s3
+@mock_sqs
+def test_should_not_send_activity_message(monkeypatch):
+    prepare_env(monkeypatch)
+    monkeypatch.setenv('SEND_BULK_ACTIVITY_MESSAGE', 'FALSE')
+
+    s3 = boto3.client('s3', region_name=_region)
+    s3_key = 'test/create-users/dce.test/users.csv'
+    meta = {
+        'requester-exid': 'admin|exid',
+        'requester-ip': '1.2.3.4',
+        'requester-city': 'Minas Tirith',
+        'requester-countrycode': 'Gondor'
+    }
+    s3.put_object(Bucket=_bucket, Key=s3_key, Body='email, name\nuser1@mail.com,User1\nuser2@mail.com,User2',
+                  Metadata=meta)
+
+    # When
+    lambda_function.lambda_handler(_event, {})
+
+    # What
+    sqs = boto3.client('sqs', region_name=_region)
+    mes1 = sqs.receive_message(QueueUrl='CREATE_USERS_SQS_QUEUE', MaxNumberOfMessages=2)
+    assert len(mes1.get('Messages')) == 2
+
+    mes2 = sqs.receive_message(QueueUrl='BULK_ACTIVITY_SQS_QUEUE', MaxNumberOfMessages=2)
+    assert 'Messages' not in mes2
+
+
+@freeze_time("2012-01-01 12:00:00")
+@mock_s3
+@mock_sqs
+def test_should_get_file_from_s3(monkeypatch):
+    prepare_env(monkeypatch)
+    s3 = boto3.client('s3', region_name=_region)
+    s3_key = 'test/create-users/dce.test/users.csv'
+    meta = {
+        'requester-exid': 'admin|exid',
+        'requester-ip': '1.2.3.4',
+        'requester-city': 'Minas Tirith',
+        'requester-countrycode': 'Gondor'
+    }
+    s3.put_object(Bucket=_bucket, Key=s3_key, Body='email, name\nuser1@mail.com,User1\nuser2@mail.com,User2',
+                  Metadata=meta)
+
+    # When
+    lambda_function.lambda_handler(_event, {})
+
+    # What
+    sqs = boto3.client('sqs', region_name=_region)
+    mes1 = sqs.receive_message(QueueUrl='CREATE_USERS_SQS_QUEUE', MaxNumberOfMessages=2)
+    payload = mes1.get('Messages')[0].get('Body')
+    assert payload == '{"payload": {"requester": {"exid": "admin|exid", "ip": "1.2.3.4", "city": "Minas Tirith", ' \
+                      '"countrycode": "Gondor"}, ' \
+                      '"realm": "dce.test", "taskId": "1325419200000-users.csv", "user": {"email": ' \
+                      '"user1@mail.com", "name": "User1"}}, "headers": {}, "properties": {}}'
+    payload = mes1.get('Messages')[1].get('Body')
+    assert payload == '{"payload": {"requester": {"exid": "admin|exid", "ip": "1.2.3.4", "city": "Minas Tirith", ' \
+                      '"countrycode": "Gondor"}, ' \
+                      '"realm": "dce.test", "taskId": "1325419200000-users.csv", "user": {"email": ' \
+                      '"user2@mail.com", "name": "User2"}}, "headers": {}, "properties": {}}'
+
+    mes1 = sqs.receive_message(QueueUrl='BULK_ACTIVITY_SQS_QUEUE', MaxNumberOfMessages=2)
+    payload = mes1.get('Messages')[0].get('Body')
+    assert payload == '{"payload": {"requester": {"exid": "admin|exid", "ip": "1.2.3.4", "city": "Minas Tirith", ' \
+                      '"countrycode": "Gondor"}, ' \
+                      '"realm": "dce.test", "taskId": "1325419200000-users.csv", "taskType": ' \
+                      '"CREATE_USER", "task": {"fileName": "users.csv", "uploadedTime": "2012-01-01T12:00:00.000000Z", ' \
+                      '"expected": 0}}, "headers": {}, "properties": {}}'
+
+    payload = mes1.get('Messages')[1].get('Body')
+    assert payload == '{"payload": {"requester": {"exid": "admin|exid", "ip": "1.2.3.4", "city": "Minas Tirith", ' \
+                      '"countrycode": "Gondor"}, ' \
+                      '"realm": "dce.test", "taskId": "1325419200000-users.csv", "taskType": ' \
+                      '"CREATE_USER", "task": {"fileName": "users.csv", "uploadedTime": "2012-01-01T12:00:00.000000Z", ' \
+                      '"expected": 2}}, "headers": {}, "properties": {}}'
+
+    # File been deleted from s3
+    s3_obj = s3.list_objects(Bucket=_bucket)
+    assert 'Contents' not in s3_obj
